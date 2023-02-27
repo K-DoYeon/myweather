e0c3bfb297329adfc0070600d0cd5957638da799
K-DoYeon <loveworud9@gmail.com>
2023-02-27 18:00:26 +0900

    0227

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+$(function(){
+    $('.threetemp-body').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $('#searchbtn').on('click',function(){
+        if($('.searchinput').hasClass('act')){
+            $('.searchinput').removeClass('act');
+        }else{
+            $('.searchinput').addClass('act');
+            $('.searchinput input').focus();
+        }
+    });
+
+
+    $('#search-city').on('keypress',function(e){
+        if(e.which == 13 && !e.shiftkey){
+            const key = $(this).val();
+            console.log(key);
+            $(this).val('');
+            $('.searchinput').removeClass('act');
+        }
+    });
+
+
+});//jquery
+
+function getWeatherLocation(){
+    navigator.geolocation.getCurrentPosition(
+        onWeatherSuccess, onWeatherError, { enableHighAccuracy: true }
+    );
+}
+
+const onWeatherSuccess = function(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(lat,lon);
+}
